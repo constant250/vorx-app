2e999fed4937eb49f478590c803ced36193a9163
jayr mendoza <BlueSky1021@users.noreply.github.com>
2022-11-24 10:56:01 +0800

    Updated login page and added BalmUI compnent framework
    • Updated the login page layout
    • Added the BalmUI component framework

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -23,7 +23,7 @@ const submit = () => {
     <GuestLayout>
         <Head title="Forgot Password" />
 
-        <div class="mb-4 text-sm text-gray-600">
+        <div class="mb-4 text-sm text-white">
             Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
         </div>
 
@@ -31,17 +31,16 @@ const submit = () => {
             {{ status }}
         </div>
 
-        <form @submit.prevent="submit">
+        <form @submit.prevent="submit" class="shadow-lg rounded-lg p-5 backdrop-blur-xl backdrop-saturate-50 bg-white/30">
             <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
+                <InputLabel for="email" value="Email" class="text-white" />
+                <TextInput id="email" type="email" class="mt-1 block w-full focus:ring-1 focus:ring-blue-500" v-model="form.email" required autofocus autocomplete="username"  />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
+                <ui-button nativeType="submit"  :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
+                    unelevated>Email Password Reset Link</ui-button>
             </div>
         </form>
     </GuestLayout>
--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,10 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 import axios from 'axios';
 import Swal from 'sweetalert2';
+import BalmUI from 'balm-ui'; // Official Google Material Components
+import BalmUIPlus from 'balm-ui-plus'; // BalmJS Team Material Components
+import 'balm-ui-css';
+import Particles from "vue3-particles";
 
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -19,6 +23,9 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(BalmUI)
+            .use(BalmUIPlus)
+            .use(Particles)
             .mount(el);
     },
 });
