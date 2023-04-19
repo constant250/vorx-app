d155b37780940e5ec1673f67c8e21cf63beecfa1
constant250 <constant.claro@gmail.com>
2023-04-19 08:48:42 +0800

    course update

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 // use App\Http\Resources\ImageResource;
 use App\Http\Requests\CreateCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
 use App\Http\Resources\CourseResource;
 use App\Models\Course;
 use App\Models\Enums\CourseStatusEnum;
@@ -19,15 +20,23 @@ class CourseController extends Controller
 {
     public static function apiRoutes()
     {
-        Route::get('courses/get-collection', [CourseController::class, 'getCollection']);
         Route::post('courses', [CourseController::class, 'create']);
+        Route::put('courses/{course}', [CourseController::class, 'update']);
+        Route::delete('courses/{course}', [CourseController::class, 'delete']);
+        Route::get('courses/{course}', [CourseController::class, 'getSingle']);
+        Route::get('courses', [CourseController::class, 'getCollection']);
     }
 
     public function getCollection(Request $request)
     {
-        $course = Course::paginate($request->input('per_page', 10));
+        $course = Course::orderBy('id','desc')->paginate($request->input('per_page', 10));
 
         return CourseResource::collection($course);
+    }
+
+    public function getSingle(Course $course)
+    {
+        return new CourseResource($course);
     }
 
     public function create(CreateCourseRequest $request)
@@ -39,6 +48,31 @@ class CourseController extends Controller
             $request->validated()['tp_code'] ?? null,
             CourseStatusEnum::memberByValue($request->validated()['status'])
         );
-    } 
+
+        return ResponseService::successCreate('Course was created.', new CourseResource($course));
+    }
+    
+    public function update(Course $course, UpdateCourseRequest $request)
+    {
+        $course = $course->Service()->update(
+            $request->validated()['name'],
+            $request->validated()['code'],
+            $request->validated()['target_enrolee'],
+            $request->validated()['tp_code'] ?? null,
+            CourseStatusEnum::memberByValue($request->validated()['status'])
+        );
+
+        return ResponseService::successCreate('Course was updated.', new CourseResource($course));
+    }
+
+    public function delete(Course $course)
+    {
+        if(!$course->Service()->delete()) {
+            return ResponseService::serverError('Course was not deleted');
+        }
+
+        return ResponseService::success('Course was deleted.');
+
+    }
 
 }
---
+++ database/migrations/2023_04_18_235336_create_students_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Schema\SchemaBuilder;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('student_id');
+            $table->string('firstname')->nullable();
+            $table->string('middlename')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('gender')->default('Male');
+            $table->date('date_of_birth')->nullable();
+            $table->string('shore_type')->default('ONSHORE');
+            $table->integer('is_active')->default(1);
+            $table->integer('is_test')->default(1);
+            $table->integer('user_id')->nullable();
+            SchemaBuilder::BelongsToUserSchemaUp($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
