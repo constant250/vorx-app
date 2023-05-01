a166af3d4d87ad8d30727c99c09cc5f21eb4c170
xyshain <xyshain@gmail.com>
2023-05-01 09:35:02 +0800

    units CRUD

---
+++ app/Http/Requests/Unit/CreateUnitRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests\Unit;
+
+use App\Models\Enums\UnitStatusEnum;
+use App\Models\Enums\UnitTypeEnum;
+use App\Models\Enums\UnitVetFlagEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateUnitRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'code' => 'required',
+            'description' => 'sometimes',
+            'unit_type' => [Rule::in(UnitTypeEnum::members())],
+            'assessment_method' => 'required',
+            'nominal_hours' => 'sometimes',
+            'scheduled_hours' => 'sometimes',
+            'training_method_id' => 'sometimes',
+            'subject_field_educ_id' => 'sometimes',
+            'vet_flag' => [Rule::in(UnitVetFlagEnum::members())],
+            'status' => [Rule::in(UnitStatusEnum::members())],
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'code' => 'Code',
+            'unit_type' => 'Unit Type',
+            'assessment_method' => 'Assessment Method',
+            'vet_flag' => 'VET Flag Status',
+            'status' => 'Status',
+        ];
+    }
+
+}
--- app/Models/Services/UnitService.php
+++ app/Models/Services/UnitService.php
@@ -4,6 +4,9 @@ namespace App\Models\Services;
 
 use App\Models\Unit;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Enums\UnitStatusEnum;
+use App\Models\Enums\UnitTypeEnum;
+use App\Models\Enums\UnitVetFlagEnum;
 
 class UnitService extends ModelService
 {
@@ -18,5 +21,63 @@ class UnitService extends ModelService
         $this->model = $unit;
     }
 
+    public static function create(
+        string $code,
+        UnitTypeEnum $unit_type,
+        string $assessment_method,
+        string $description = null,
+        int $nominal_hours = 0,
+        int $scheduled_hours = 0,
+        int $training_method_id = null,
+        int $subject_field_educ_id = null,
+        UnitVetFlagEnum $vet_flag,
+        UnitStatusEnum $status
+    )
+    {
+        $unit = new Unit;
+        $unit->code = $code;
+        $unit->unit_type = $unit_type;
+        $unit->assessment_method = $assessment_method;
+        $unit->description = $description;
+        $unit->nominal_hours = $nominal_hours;
+        $unit->scheduled_hours = $scheduled_hours;
+        $unit->training_method_id = $training_method_id;
+        $unit->subject_field_educ_id = $subject_field_educ_id;
+        $unit->vet_flag = $vet_flag;
+        $unit->status = $status;
+        $unit->user_id = Auth::user()->id;
+        $unit->account_id = Auth::user()->account_id;
+        $unit->save();
+
+        return $unit;
+    }
+
+    public function update(
+        string $code,
+        UnitTypeEnum $unit_type,
+        string $assessment_method,
+        string $description = null,
+        int $nominal_hours = 0,
+        int $scheduled_hours = 0,
+        int $training_method_id = null,
+        int $subject_field_educ_id = null,
+        UnitVetFlagEnum $vet_flag,
+        UnitStatusEnum $status
+    )
+    {
+        $this->unit->code = $code;
+        $this->unit->unit_type = $unit_type;
+        $this->unit->assessment_method = $assessment_method;
+        $this->unit->description = $description;
+        $this->unit->nominal_hours = $nominal_hours;
+        $this->unit->scheduled_hours = $scheduled_hours;
+        $this->unit->training_method_id = $training_method_id;
+        $this->unit->subject_field_educ_id = $subject_field_educ_id;
+        $this->unit->vet_flag = $vet_flag;
+        $this->unit->status = $status;
+        $this->unit->save();
+
+        return $this->unit->fresh();
+    }
    
 }
