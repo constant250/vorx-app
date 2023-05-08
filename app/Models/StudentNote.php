da8a0a5e07e37005698604a16070f49f412f527e
constant250 <constant.claro@gmail.com>
2023-05-08 15:44:09 +0800

    notes

---
+++ app/Models/StudentNote.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class StudentNote extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        'deleted_at' => 'datetime',
+    ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+}
